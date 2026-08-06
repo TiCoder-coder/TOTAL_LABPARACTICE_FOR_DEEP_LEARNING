a66b550baf14fdee0530bfed6c97218ab07a89d7
Tuvienn <nguyenthituvien2005@gmail.com>
2026-08-07 02:54:15 +0700

    Refactor Notebook 6 for Train-Validation analysis and Overfitting Diagnostics

--- test_split.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-df = pd.read_csv("total_practice/practice_2_2/data/manifests/canonical_split/split_manifest.csv")
-df = df[(df['is_generated'] == False) & (df['quarantined'] == False) & (df['use_for_model'] == True)].copy()
-from total_practice.practice_2_2.src.practice_2_2.split_v2_pipeline import create_effective_groups, assign_splits
-df = create_effective_groups(df)
-df = assign_splits(df)
-print(df['split'].value_counts())
